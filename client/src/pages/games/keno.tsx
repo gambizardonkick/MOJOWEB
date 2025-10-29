bb93cf8b2c02773659e9862b440a25aef7f1b190
gambizardonkick <gambizardonkick@gmail.com>
2025-10-29 18:07:56 +0900

    Update keno.tsx

--- client/src/pages/games/keno.tsx
+++ client/src/pages/games/keno.tsx
@@ -98,7 +98,7 @@ function KenoGamePage() {
           const tileNumber = data.drawnNumbers[currentIndex];
           setRevealedNumbers(prev => [...prev, tileNumber]);
           currentIndex++;
-          animationTimerRef.current = setTimeout(revealNext, 100);
+          animationTimerRef.current = setTimeout(revealNext, 200);
         } else {
           setTimeout(() => {
             const fullResult = {
